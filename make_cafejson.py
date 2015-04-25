eeb5af46336e642dbcb983cc326303b823dcf3f0
Mogu programming <kbu94984@gmail.com>
2015-04-25 23:43:40 +0900

    add cafejson style

---
+++ make_cafejson.py
@@ -0,0 +1,14 @@
+#!/bin/usr/env python
+#coding:utf-8
+#editor:ono
+#This script makes json-style-input-file from cafemol input file.
+
+
+class MakeCafeJson:
+    def __init__(self):
+        pass
+
+    
+if __name__ == "__main__":
+    pass
+
--- submit_queue.py
+++ submit_queue.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python
+#coding:utf-8
+#editor:ono
+#This script makes input files and submits queue.
+
+import sys
+import subprocess
+
+if __name__ == "__main__":
+    pass
