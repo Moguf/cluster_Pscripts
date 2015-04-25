d0c37a40bda3049e733445523f27253ef9965937
rei <ono@rei.local>
2015-04-26 06:19:58 +0900

    refine all sciprts

--- make_queue.py
+++ make_queue.py
@@ -2,6 +2,8 @@
 # coding:utf-8
 import sys
 import subprocess
+import time
+import os
 
 import read_inp
 import read_sh
@@ -19,21 +21,28 @@ class GoQueue:
     def makeInp(self):
         inp_instance=read_inp.InpFile(self.inpfile)
         sh_instance=read_sh.ReadSh(self.shfile)
-
+        
+        _counter=0
         for index,data in enumerate(xrange(300,445,5)):
             #for inp
             for seed in range(1,11):
+                inpfile="mvm"+str(data)+"r"+str(seed)+".inp"
                 inp_instance.filename('mon'+str(data)+str(seed))
                 inp_instance.temp(float(str(data)+".0"))
                 inp_instance.seed(seed)
-                inp_instance.write('out.inp')
-                #inp_instance.write('mon'+str(data)+str(seed)+".inp")
+                inp_instance.write(inpfile)
+                print os.path.exists("./"+inpfile)
                 #for sh
+                shfile="mvm"+str(data)+"r"+str(seed)+".sh"
                 sh_instance.change_name("mvm"+str(data)+"r"+str(seed))
-                sh_instance.write("out.sh")
+                sh_instance.change_inp(inpfile)
+                sh_instance.write(shfile)
                 #do shell
-                subprocess.call('qsub out.sh',shell=True)
-        
+                subprocess.call('qsub '+shfile,shell=True)
+                #subprocess.call('rm '+inpfile+" "+shfile,shell=True)
+                
+                _counter+=1
+                
 if __name__=="__main__":
     #How to use
     #scirpt.py template.inp template.sh
--- read_sh.py
+++ read_sh.py
@@ -15,15 +15,26 @@ class ReadSh:
         for index,iline in enumerate(self.data):
             if "#$ -N"==iline[:5]:
                 self.data[index]="#$ -N %s\n"% str(name)
-    
 
+                
+    def change_inp(self,name):
+        for index,iline in enumerate(self.data):
+            if iline[0]=="/":
+                ilist=iline.split()
+                _ilist=ilist[1].split("/")
+                _ilist[-1]=name
+                ilist[1]="/".join(_ilist)
+                self.data[index]=" ".join(ilist)
+
+            
     def write(self,ofile):
         otxt=''
         for iline in self.data:
             otxt+=iline
         odata=open(ofile,'w')
         odata.write(otxt)
-
+        odata.close()
+        
 if __name__=="__main__":
     test=ReadSh(sys.argv[1])
     test.main()
